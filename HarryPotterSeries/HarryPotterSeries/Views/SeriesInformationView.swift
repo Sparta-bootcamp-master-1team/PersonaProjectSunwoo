dad09e20acac030234711370cb68c47374ef4181
Sn8Ch0 <whtjsdn98@gmail.com>
2025-03-31 20:15:38 +0900

    refactor: seriesInfoStackView Constraints 수정

--- HarryPotterSeries/HarryPotterSeries/Views/SeriesInformationView.swift
+++ HarryPotterSeries/HarryPotterSeries/Views/SeriesInformationView.swift
@@ -131,8 +131,8 @@ class SeriesInformationView: UIView {
         }
         
         seriesInfoStackView.snp.makeConstraints {
-            $0.leading.trailing.top.equalToSuperview()
-            $0.bottom.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.top.equalToSuperview()
         }
     }
     
